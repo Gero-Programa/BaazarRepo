71d2ac7145d8f31572ca0c4b89293c34e5b824b4
Gero-Programa <geronimolopardo@gmail.com>
2023-05-21 11:47:09 -0300

    actualizamos

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 import express from 'express';
+import passport from 'passport';
+import db from './db.js';
+import path from 'path'
+
 
 ////////////////////////////////////////////////////////////////////////////////
-class BaazarBackend {
+class BaazarBackendServer {
   constructor() {
     const app = express();
     app.use(express.json());
     app.use(express.static('public'));
     app.listen(3000, () => console.log('Listening on port 3000'));    
+    const xd = path.join(__dirname, 'public');
+    app.get('/', function(req, res) {
+      res.sendFile(path.join(xd, 'login.html'));
+    });
+    app.get('/logindone', function(req, res) {
+    res.sendFile(path.join(xd, 'index.html'));
+    });
+
   }
 }
 
-new BaazarBackend();
+new BaazarBackendServer();
